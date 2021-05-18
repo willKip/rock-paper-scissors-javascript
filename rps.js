bea29b2f853b907af3dca5540736cdf4c0136cc1
willKip <coldbrushl@gmail.com>
2021-05-18 16:01:01 -0400

    Introduce scoring and game resets
    Scores are now recorded, and if player or computer
    reaches 5 points, the winner is announced and the
    game and scores are reset.

--- rps.js
+++ rps.js
@@ -1,7 +1,7 @@
 // Returns a string from 'rock', 'paper', or 'scissors' randomly.
 function computerPlay() {
     const options = ["Rock", "Paper", "Scissors"];
-    return options[Math.floor(Math.random() * 3)];  // return random integer from 0 to 2; chooses random option
+    return options[Math.floor(Math.random() * options.length)];  // return random integer from 0 to 2; chooses random option
 }
 
 function resultLogic(playerSelection, computerSelection) {
@@ -35,19 +35,49 @@ function playRound(playerInput) {
         case "win":
             resultWindow.setAttribute("style", "background-color: turquoise");
             resultWindow.textContent = `You win. ${ playerInput } beats ${ computerInput }.`;
+            playerWins++;
             break;
         case "lose":
             resultWindow.setAttribute("style", "background-color: orange");
             resultWindow.textContent = `You lose. ${ computerInput } beats ${ playerInput }.`;
+            computerWins++;
             break;
         case "tie":
             resultWindow.setAttribute("style", "background-color: gray");
             resultWindow.textContent = "It's a tie.";
             break;
     }
+
+    updateScores()
+
+    if (playerWins >= 5) {
+        resultWindow.setAttribute("style", "background-color: turquoise");
+        resultWindow.textContent = "YOU reached 5 points! You win!";
+        resetScores();
+    } else if (computerWins >= 5) {
+        resultWindow.setAttribute("style", "background-color: orange");
+        resultWindow.textContent = "COM reached 5 points! You lose!";
+        resetScores();
+    }
 }
 
-const resultWindow = document.querySelector("#resultsWindow");
+function updateScores() {
+    scoreWindow.textContent = `Score: YOU - ${playerWins}, COM - ${computerWins}`;
+}
+
+function resetScores() {
+    playerWins = 0;
+    computerWins = 0;
+    updateScores();
+}
+
+let playerWins = 0;
+let computerWins = 0;
+
+const scoreWindow = document.querySelector("#scoreWindow");
+const resultWindow = document.querySelector("#resultWindow");
+
+updateScores();
 
 // Defining button behaviour
 const rockBtn = document.querySelector("#rockSelect");
